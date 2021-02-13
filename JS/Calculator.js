9d52ce132288e621bcf8ebbb8eaefbe6cc2187a4
Peerzada Usmaan <70443244+pu-coder@users.noreply.github.com>
2021-02-13 23:57:02 +0530

    Add files via upload

---
+++ JS/Calculator.js
@@ -0,0 +1,28 @@
+// program for a simple calculator
+
+// take the operator input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// take the operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+// using if...else if... else
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+// display the result
+console.log(`${number1} ${operator} ${number2} = ${result}`);
+document.write("<h3 style='font-size: 30px;'> Answer: </h3>" + `<h3 style='text-align: left;'> ${number1} ${operator} ${number2} = ${result} </h3>`);
